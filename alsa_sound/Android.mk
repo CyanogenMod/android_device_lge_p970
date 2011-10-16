e7f7f4104d44b2f64e3c6ba7e43817384656097d
Ricardo Cerqueira <github@cerqueira.org>
2011-10-16 23:06:59 +0100

    p970: Add libaudiopolicy
    The TARGET_PROVIDES_LIBAUDIO config requires the device to provide
    libaudiopolicy as well

--- alsa_sound/Android.mk
+++ alsa_sound/Android.mk
@@ -42,4 +42,28 @@ endif
 
   include $(BUILD_SHARED_LIBRARY)
 
+# This is the ALSA audio policy manager
+
+  include $(CLEAR_VARS)
+
+  LOCAL_CFLAGS := -D_POSIX_SOURCE
+
+ifeq ($(BOARD_HAVE_BLUETOOTH),true)
+    LOCAL_CFLAGS += -DWITH_A2DP
+endif
+
+  LOCAL_SRC_FILES := AudioPolicyManagerALSA.cpp
+
+  LOCAL_MODULE := libaudiopolicy
+  LOCAL_MODULE_TAGS:= optional
+
+  LOCAL_WHOLE_STATIC_LIBRARIES += libaudiopolicybase
+
+  LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    libutils \
+    libmedia
+
+  include $(BUILD_SHARED_LIBRARY)
+
 endif
