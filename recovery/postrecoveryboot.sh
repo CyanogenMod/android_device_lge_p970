32ac4807b2bae3fcf8ed0ae6f036077970deb7c9
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-09-22 18:31:39 +0100

    p970: Fix recovery flags with the new kernel

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -3,9 +3,6 @@
 sleep 3
 
 ## Reset recovery flags
-#boot-recovery
-dd if=/dev/zero of=/dev/block/mmcblk0p12 seek=512 count=13 bs=1
-#recovery\n--wipe-data
-dd if=/dev/zero of=/dev/block/mmcblk0p12 seek=576 count=20 bs=1
+cat /dev/zero > /dev/block/mmcblk0p12
 
-echo 0 > /sys/devices/platform/i2c_omap.2/i2c-2/2-001a/blink_enable
+echo 0 > /sys/devices/platform/omap/omap_i2c.2/i2c-2/2-001a/blink_enable
