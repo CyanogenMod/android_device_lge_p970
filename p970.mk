09c5a2d2661ff8b95f136f87f98ef45540196a46
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-09-28 00:54:05 +0100

    p970: Use the prebuilt dalvik config for HDPI/512

--- p970.mk
+++ p970.mk
@@ -126,6 +126,8 @@ PRODUCT_PACKAGES += \
 
 $(call inherit-product, build/target/product/full.mk)
 
+$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
+
 PRODUCT_AAPT_CONFIG := normal hdpi
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
